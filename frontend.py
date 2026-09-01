5c619636078c2f297beee48edc7e7a7641398b7b
Affan Bin Inam <affanbininam.info@gmail.com>
2026-09-01 02:21:05 +0200

    Modified frontend file

--- frontend.py
+++ frontend.py
@@ -8,61 +8,6 @@ API_URL = "http://127.0.0.1:8000/predict"
 st.title("📊 Customer Churn Predictor")
 st.markdown("Enter a customer's details to estimate their likelihood of churning.")
 
-# input fields
-# gender = st.selectbox("Gender", options=['Male', 'Female'])
-# SeniorCitizen = st.radio('Senior Citizen', options=['Yes', 'No'])
-# Partner = st.radio('Parter', options=['Yes', 'No'])
-# Dependents = st.radio('Dependents', options=['Yes', 'No'])
-# PhoneService = st.radio('Phone Service', options=['Yes', 'No'])
-# MultipleLines = st.selectbox('Multiple Lines', options=['Yes', 'No', 'No Phone Service'])
-# InternetService = st.selectbox('Internet Service', options=['DSL', 'Fiber optic', 'No'])
-# OnlineSecurity  = st.radio('Online Security', options=['Yes', 'No'])
-# OnlineBackup = st.radio('Online Backup', options=['Yes', 'No'])
-# DeviceProtection = st.radio('Device Protection', options=['Yes', 'No'])
-# TechSupport = st.radio('Tech Support', options=['Yes', 'No'])
-# StreamingTV = st.radio('Streaming TV', options=['Yes', 'No'])
-# StreamingMovies = st.radio('Streaming Movies', options=['Yes', 'No'])
-# Contract = st.selectbox('Contract', options=['Month-to-month', 'One year', 'Two year'])
-# PaperlessBilling = st.radio('Paperless Billing', options=['Yes', 'No'])
-# PaymentMethod = st.selectbox('Payment Method', options=['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
-#        'Credit card (automatic)'])
-# Tenure = st.number_input('Tenure', min_value= 0, value= 0)
-# MonthlyCharges = st.number_input('Monthly Charges', min_value= 0, value= 0)
-
-
-
-# if st.button('Pridict!!'):
-#     input_data = {
-#         'gender': gender,
-#         'SeniorCitizen': SeniorCitizen,
-#         'Partner': Partner,
-#         'Dependents': Dependents,
-#         'PhoneService': PhoneService,
-#         'MultipleLines': MultipleLines,
-#         'InternetService': InternetService,
-#         'OnlineSecurity': OnlineSecurity,
-#         'OnlineBackup': OnlineBackup,
-#         'DeviceProtection': DeviceProtection,
-#         'TechSupport': TechSupport,
-#         'StreamingTV': StreamingTV,
-#         'StreamingMovies': StreamingMovies,
-#         'Contract': Contract,
-#         'PaperlessBilling': PaperlessBilling,
-#         'PaymentMethod': PaymentMethod,
-#         'Tenure': Tenure,
-#         'MonthlyCharges': MonthlyCharges  
-#         }
-
-    # try:
-    #     response = requests.post(API_URL, json=input_data)
-    #     if response.status_code == 200:
-    #         result = response.json()
-    #         st.success(f"The verdict on, if the customer is likel to churn is: **{result['churn_prediction']}** \nWith Probability: **{result['churn_probablility']}**")
-    #     else:
-    #         st.error(f"API error: {response.status_code} - {response.text}")
-    # except requests.exceptions.ConnectionError:
-    #     st.error("Couldn't Connect to FastAPI Server, Make sure it's running on port 8000")
-
 
 with st.form("churn_form"):
     st.subheader("Customer Profile")
